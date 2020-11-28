0bdca995d19e9789ffe453ecc940fde9cba7e61a
rhinehardtc <rhinehardtc@gmail.com>
2020-11-28 03:30:52 -0600

    finished more section

---
+++ demo-site/src/AndMoreSection.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import Sketch from "react-p5";
+
+const AndMoreSection = () => {
+
+    const pStyle = {'font-size': '6vmin'};
+    const divStyle = {width: '50%'}
+
+    return(
+        <>
+            <div className="intro_div">
+                <h2>Always Learning</h2>
+            </div>
+            <div className="more_section">
+                <p style={pStyle}>Listed below are some other technologies I've worked with.</p>
+                <div style={divStyle}>
+                    <ul>
+                        <li>JSON</li>
+                        <li>Web Audio API</li>
+                        <li>GamePad API</li>
+                        <li>p5.js</li>
+                        <li>Lodash</li>
+                        <li>Git</li>
+                        <li>Bit</li>
+                        <li>Sessions</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AndMoreSection;
